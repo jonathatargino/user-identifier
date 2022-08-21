7a2515365a50646d584b84872f331db1a047db14
Jonatha Targino <j.oliveiratargino@hotmail.com>
2022-08-21 15:44:42 -0300

    initial commit

---
+++ script.js
@@ -0,0 +1,43 @@
+const signUpForm = document.querySelector("#form")
+const signUpWindow = document.querySelector("#sign-up")
+const welcomeWindow = document.querySelector("#welcome")
+const logoutBtn = document.querySelector("#logout")
+const welcome = document.querySelector("#welcome-text");
+const password = document.querySelector("#password");
+const userIcon = document.querySelector(".signUp-icon");
+const structure = document.querySelector(".structure");
+const backgroundImg = document.querySelector(".side-img")
+
+const identify = () => {
+    const name = localStorage.getItem("name");
+    if(name){
+        welcome.innerHTML = `Seja bem-vindo, ${name}` 
+        signUpWindow.style.display = "none";
+        welcomeWindow.style.display = "block"
+        userIcon.style.display = "none"
+        structure.style.gridTemplateAreas = '"img-nav img-nav"';
+    } else{
+        signUpWindow.style.display = "block";
+        welcomeWindow.style.display = "none"
+        userIcon.style.display = "block"
+        structure.style.gridTemplateAreas = '"img-nav sign-up"';
+        
+    }
+}
+
+signUpForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const nameInput = document.querySelector("#name")
+    localStorage.setItem("name", nameInput.value)
+    
+    identify()
+    signUpForm.reset()
+})
+
+logoutBtn.addEventListener("click", (event) => {
+    localStorage.clear()
+    identify()
+})
+
+identify()
